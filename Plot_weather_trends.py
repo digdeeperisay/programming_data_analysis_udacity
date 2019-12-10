97b415972bfb532d77b787fc77a9ce8257dac239
digdeeperisay <48805780+digdeeperisay@users.noreply.github.com>
2019-12-10 18:30:53 -0500

    blank file with just high level steps needed

---
+++ Plot_weather_trends.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  5 19:55:43 2019
+@author: Prajamohan
+Purpose: Plotting Weather trends as part of the Data Analyst course on Udacity
+High level steps:
+    Download the CSV files for the entire world and interested cities
+    Compare and understand range of years in each etc.
+    Plot them side by side and draw insights
+    Write up a report with findings
+"""
+
+import matplotlib.pyplot as plt
